2e54e3c3ddcb69a3e450968c7b9a28dae692f360
itsAmyHong <itsAmyHong@gmail.com>
2023-09-22 12:57:39 -0500

    Added initial chatbot functionality

---
+++ src/Chat.js
@@ -0,0 +1,108 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const senderName = 'You';
+  const botName = 'Virtual TA';
+  
+  const messageContainerRef = useRef(null);
+
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleInputKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = () => {
+    if (inputText.trim() === '') return;
+
+    // Create a new user message
+    const userMessage = {
+      text: inputText,
+      sender: senderName,
+    };
+
+    // Update the messages array with the new user message
+    setMessages([...messages, userMessage]);
+
+    // Clear the input field
+    setInputText('');
+  };
+
+  useEffect(() => {
+    // Check if the latest message is from the user
+    const latestMessage = messages[messages.length - 1];
+    if (latestMessage && latestMessage.sender === senderName) {
+      const userMessageText = latestMessage.text;
+
+      const fetchBotResponse = async () => {
+        // Fetch data from the backend
+        const responseText = await simulateBackendResponse(userMessageText);
+
+        // Create a new bot message
+        const botResponse = {
+          text: responseText,
+          sender: botName,
+        };
+
+        // Update the messages array with the bot's response
+        setMessages([...messages, botResponse]);
+      };
+
+      fetchBotResponse();
+    }
+  }, [messages]);
+
+  const simulateBackendResponse = async (userMessageText) => {
+    // Simulate fetching data from the backend
+    // Make API call here!
+    return userMessageText; // For testing the bot's response echos user's input
+  };
+
+  useEffect(() => {
+    // Scroll to the bottom of the message container
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  return (
+    <div className="chat-container">
+
+      <div className="message-container" ref={messageContainerRef}>
+        {messages.slice().reverse().map((message, index) => (
+          <div
+            key={index}
+            className={`message ${message.sender === senderName ? 'user' : 'bot'}`}
+          >
+            {message.sender === senderName ? (
+              <strong>{message.sender}: </strong>
+            ) : (
+              <strong>{botName}: </strong>
+            )}
+            {message.text}
+          </div>
+        ))}
+      </div>
+
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Type a message..."
+          value={inputText}
+          onChange={handleInputChange}
+          onKeyPress={handleInputKeyPress}
+        />
+      </div>
+
+    </div>
+  );
+};
+
+export default Chat;
