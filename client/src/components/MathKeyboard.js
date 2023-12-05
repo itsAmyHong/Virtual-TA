8a9dd14e9c7cb64eba2301260179f9f08fb76fce
Dhanush Mahesh <68971840+dhanush-mahesh@users.noreply.github.com>
2023-12-05 12:20:47 -0600

    Update MathKeyboard.js

--- client/src/components/MathKeyboard.js
+++ client/src/components/MathKeyboard.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useRef } from 'react';
 
-const MathKeyboard = () => {
+const MathKeyboard = ({ onSymbolClick }) => {
   const calculatorRef = useRef(null);
 
   useEffect(() => {
     if (window.Desmos && calculatorRef.current) {
-      window.Desmos.Calculator(calculatorRef.current);
+      const calculator = window.Desmos.Calculator(calculatorRef.current);
+
+      calculator.observeEvent('change', () => {
+        const expressions = calculator.getState().expressions.list;
+
+        if (expressions && expressions.length > 0) {
+          const latestExpression = expressions[expressions.length - 1];
+
+          if (latestExpression.latex) {
+            // Send the LaTeX representation of the symbol
+            onSymbolClick(latestExpression.latex);
+          }
+        }
+      });
     }
-  }, []);
+  }, [onSymbolClick]);
 
   return (
-    <div ref={calculatorRef} style={{ width: '900px', height: '500px' }}></div>
+    <div ref={calculatorRef} style={{ width: '900px', height: '350px' }}></div>
   );
 };
 
